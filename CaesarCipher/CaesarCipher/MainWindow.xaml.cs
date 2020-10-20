695df76386f96ec9aa92d14a740b0d6089e3b060
djarosz-hub <jaroszdamian9@gmail.com>
2020-10-20 23:08:40 +0200

    last fix

--- CaesarCipher/CaesarCipher/MainWindow.xaml.cs
+++ CaesarCipher/CaesarCipher/MainWindow.xaml.cs
@@ -85,7 +85,7 @@ namespace CaesarCipher
             string output = "";
             for (int i = 0; i < input.Length; i++)
             {
-                if (char.IsLetterOrDigit(input[i]))
+                if (char.IsLetter(input[i]))
                 {
                     output += char.ToUpper(input[i]);
                 }
