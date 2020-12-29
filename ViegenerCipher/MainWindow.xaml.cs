17929edbece32e2ec64d11c17415c08877ed4104
djarosz-hub <jaroszdamian9@gmail.com>
2020-12-29 02:55:44 +0100

    creating app

--- ViegenerCipher/MainWindow.xaml.cs
+++ ViegenerCipher/MainWindow.xaml.cs
@@ -20,9 +20,178 @@ namespace ViegenerCipher
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly char[] Alphabet = new char[35] { 'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż' };
+        readonly char[,] CipherFieldArr = new char[35, 35];
         public MainWindow()
         {
             InitializeComponent();
+            FillField(Alphabet, CipherFieldArr);
+        }
+        static void FillField(char[] alphabet, char[,] field)
+        {
+            int len = alphabet.Length;
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    field[i, j] = alphabet[(i + j) % 35];
+                }
+            }
+        }
+        static string ParseKeyWithText(string key, string text)
+        {
+            string output = "";
+            int keyLength = key.Length;
+            byte spacesCounter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    output += ' ';
+                }
+                if (char.IsLetter(text[i]))
+                {
+                    output += key[(i - spacesCounter) % keyLength];
+                }
+                else
+                    spacesCounter++;
+
+            }
+            return output;
+        }
+        static string Encrypt(string text, string keyToEncrypt, char[,] field)
+        {
+            string output = "";
+            int textLength = text.Length;
+            int fieldLength = field.GetLength(0);
+            for (int i = 0; i < textLength; i++)
+            {
+                if (keyToEncrypt[i] == ' ')
+                {
+                    output += ' ';
+                    continue;
+                }
+                int xPos = -1;
+                int yPos = -1;
+                for (int x = 0; x < fieldLength; x++)
+                {
+                    if (text[i] == field[x, 0])
+                    {
+                        xPos = x;
+                        break;
+                    }
+                }
+                for (int y = 0; y < fieldLength; y++)
+                {
+                    if (keyToEncrypt[i] == field[0, y])
+                    {
+                        yPos = y;
+                        break;
+                    }
+                }
+                if (xPos != -1 && yPos != -1)
+                {
+                    output += field[xPos, yPos];
+                }
+                else
+                    throw new Exception($"wrong index at text: {output} xPos:{xPos}, yPos:{yPos}");
+            }
+
+            return output;
+        }
+        static string Decrypt(string key, string encryptedText, char[,] field)
+        {
+            string output = "";
+            int textLength = encryptedText.Length;
+            int fieldLength = field.GetLength(0);
+            for (int i = 0; i < textLength; i++)
+            {
+                int xPos = -1;
+                int yPos = -1;
+                if (key[i] == ' ')
+                {
+                    output += ' ';
+                    continue;
+                }
+                for (int x = 0; x < fieldLength; x++)
+                {
+                    if (field[x, 0] == key[i])
+                    {
+                        xPos = x;
+                        break;
+                    }
+                }
+                for (int y = 0; y < fieldLength; y++)
+                {
+                    if (field[xPos, y] == encryptedText[i])
+                    {
+                        yPos = y;
+                        break;
+                    }
+                }
+                if (xPos != -1 && yPos != -1)
+                {
+                    output += field[0, yPos];
+                }
+                else
+                    throw new Exception($"wrong index at text: {output} xPos:{xPos}, yPos:{yPos}");
+
+            }
+            return output;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textToEncrypt = EncryptInput.Text;
+            string key = EncryptKey.Text;
+            FormatValues(key, textToEncrypt, out string formattedKey, out string formattedText);
+            if (formattedText == "" || formattedKey == "")
+            {
+                EncryptOutputTextBox.Text = "Either key or input text can't be empty or contain non letter characters";
+                return;
+            }
+            string parsedKey = ParseKeyWithText(formattedKey, formattedText);
+            string encryptedText = Encrypt(formattedText, parsedKey, CipherFieldArr);
+            EncryptOutputTextBox.Text = encryptedText;
+            DecryptInput.Text = encryptedText;
+            DecryptKey.Text = parsedKey;
+        }
+        static void FormatValues(string key, string text, out string formattedKey, out string formattedText)
+        {
+            formattedText = FormatAndRemoveUnwantedChars(text);
+            string tempKey = FormatAndRemoveUnwantedChars(key);
+            formattedKey = RemoveSpacesFromInside(tempKey);
+        }
+        static string RemoveSpacesFromInside(string key)
+        {
+            string output = "";
+            for(int i = 0; i < key.Length; i++)
+            {
+                if (char.IsLetter(key[i]))
+                    output += key[i];
+            }
+            return output;
+        }
+        static string FormatAndRemoveUnwantedChars(string input)
+        {
+            string text = input.Trim();
+            string output = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    output += ' ';
+                    continue;
+                }
+                if (char.IsLetter(text[i]))
+                    output += text[i];
+            }
+            return output.ToUpper();
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
